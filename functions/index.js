58a140dc991996c28001a5fe5a2d0db475095629
Suyesh Bhandari <suyeshb@gmail.com>
2019-02-19 21:04:03 -0500

    .

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-const functions = require('firebase-functions');
+"use strict";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const glob = require("glob");
+const camelCase = require("camelcase");
+const files = glob.sync("./**/*.f.js", {
+  cwd: __dirname,
+  ignore: "./node_modules/**"
+});
+for (let f = 0, fl = files.length; f < fl; f++) {
+  const file = files[f];
+  const functionName = camelCase(
+    file
+      .slice(0, -5)
+      .split("/")
+      .join("_")
+  );
+  if (
+    !process.env.FUNCTION_NAME ||
+    process.env.FUNCTION_NAME === functionName
+  ) {
+    exports[functionName] = require(file);
+  }
+}
